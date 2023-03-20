1384967df341342f3d3ed96ba89ab377465918fa
artemijsem <A.Sementsenko1@uni.brighton.ac.uk>
2023-03-20 12:39:18 +0000

    Added chart and data display for each module. Changed class names for <p> elements in each module. ISSUE: Charts inside module <div> elements are hidden

--- Scripts/Details.js
+++ Scripts/Details.js
@@ -9,44 +9,74 @@ function UpdateDetails()
 
 
   var allMeetings = 0;
+  var attended = 0;
+  var missed = 0;
+  var currentAllMeetings = 0;
+  var currentAttended = 0;
+  var attendanceData;
   let modules = jsonFile.data[studentID].enlisted_modules;
   for(let i = 0; i < modules.length; i++)
   {
-    allMeetings += jsonFile.data[studentID].attendance[modules[i]].meetings;
+    currentAllMeetings = jsonFile.data[studentID].attendance[modules[i]].meetings;
+    currentAttended = jsonFile.data[studentID].attendance[modules[i]].entries_num;
+    missed = currentAllMeetings - currentAttended;
+
+    // Create chart for each module
+    let chartData = [currentAttended, missed];
+    createChart(modules[i], chartData);
+
+    // Manage data for each module
+    let textClassAttended = ".HomeContainer_ModuleSessionInformation_Attended_" + modules[i]; 
+    let textClassAbsent = ".HomeContainer_ModuleSessionInformation_Absent_" + modules[i];
+    document.querySelector(textClassAttended).textContent = "Attended: " + currentAttended;
+    document.querySelector(textClassAbsent).textContent = "Absent: " + missed;
+
+    // Update overall attendance data
+    allMeetings += currentAllMeetings;
+    attended += currentAttended;
+
   }
     
-  var attended = 0;
-  for(let i = 0; i < modules.length; i++)
-  {
-    attended += jsonFile.data[studentID].attendance[modules[i]].entries_num;
-  }
+  // Update variable to apply for overall attendance
+  missed = allMeetings - attended;
+  document.querySelector(".Attended_Text").textContent = "Attended: " + attended;
+  document.querySelector(".Absent_Text").textContent = "Absent: " + missed;
+  document.querySelector(".TotalSessions_Text").textContent = "Total Sessions: " + allMeetings;
 
-   var missed = allMeetings - attended;
-   document.querySelector(".Attended_Text").textContent = "Attended: " + attended;
-   document.querySelector(".Absent_Text").textContent = "Absent: " + missed;
-   document.querySelector(".TotalSessions_Text").textContent = "Total Sessions: " + allMeetings;
-   var xValues = ["Attended", "Missed"];
-   var yValues = [attended, missed];
-   var barColors = [
-   "#1e7145",
-   "#b91d47"
-   ];
 
-   new Chart("OverallAttendanceDonut", {
-       type: "doughnut",
-       data: {
-         labels: xValues,
-         datasets: [{
-           backgroundColor: barColors,
-           data: yValues
-         }]
-       },
-       options: {
-           legend: {
-               display: false
-           }
-       }
-     });
+  attendanceData = [attended, missed];
+  let name = "OverallAttendanceDonut";
+  createChart(name, attendanceData)
+
+  
+
+}
+
+function createChart(chartName, pData)
+{
+  new Chart(chartName, {
+    type: "doughnut",
+    data: {
+      labels: ["Attended", "Missed"],
+      datasets: [{
+        backgroundColor: [
+          "#1e7145",
+          "#b91d47"
+          ],
+        data: pData
+      }]
+    },
+    options: {
+        legend: {
+            display: false
+        }
+    }
+  });
+}
+
+function getAllMeetings(jsonFile, studentID, module)
+{
+  return jsonFile.data[studentID].attendance[module].meetings;
 }
 
 window.onload = UpdateDetails();
--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -10,35 +10,47 @@ HC_ModuleThree.addEventListener("click", ToggleModuleThreeContent)
 
 // Forces the display to be in a "hidden" state so you don't need to double-click.
 let HomeContainer_ModuleOne_Content = document.querySelector(".HomeContainer_ModuleOne_Content")
+let HomeContainer_ModuleOne_AttendanceDonut = document.querySelector(".HomeContainer_ModuleOne_AttendanceDonut")
 HomeContainer_ModuleOne_Content.style.display = "none";
+HomeContainer_ModuleOne_AttendanceDonut.style.display = "none";
 
 let HomeContainer_ModuleTwo_Content = document.querySelector(".HomeContainer_ModuleTwo_Content")
 HomeContainer_ModuleTwo_Content.style.display = "none";
+let HomeContainer_ModuleTwo_AttendanceDonut = document.querySelector(".HomeContainer_ModuleTwo_AttendanceDonut")
+HomeContainer_ModuleTwo_AttendanceDonut.style.display = "none";
 
 let HomeContainer_ModuleThree_Content = document.querySelector(".HomeContainer_ModuleThree_Content")
 HomeContainer_ModuleThree_Content.style.display = "none";
+let HomeContainer_ModuleThree_AttendanceDonut = document.querySelector(".HomeContainer_ModuleThree_AttendanceDonut")
+HomeContainer_ModuleThree_AttendanceDonut.style.display = "none";
 
 // This can be duplicated per module. I think there has to be a better way to do this.
 function ToggleModuleOneContent()
 {
     if (HomeContainer_ModuleOne_Content.style.display === "none")
-        HomeContainer_ModuleOne_Content.style.display = "flex";
+{    HomeContainer_ModuleOne_Content.style.display = "flex";
+    HomeContainer_ModuleOne_AttendanceDonut.style.display = "flex"}
     else
-        HomeContainer_ModuleOne_Content.style.display = "none";
+{        HomeContainer_ModuleOne_Content.style.display = "none";
+        HomeContainer_ModuleOne_AttendanceDonut.style.display = "none";}
 }
 
 function ToggleModuleTwoContent()
 {
     if (HomeContainer_ModuleTwo_Content.style.display === "none")
-        HomeContainer_ModuleTwo_Content.style.display = "flex";
+{    HomeContainer_ModuleTwo_Content.style.display = "flex";
+    HomeContainer_ModuleTwo_AttendanceDonut.style.display = "flex"}
     else
-        HomeContainer_ModuleTwo_Content.style.display = "none";
+{        HomeContainer_ModuleTwo_Content.style.display = "none";
+        HomeContainer_ModuleTwo_AttendanceDonut.style.display = "none";}
 }
 
 function ToggleModuleThreeContent()
 {
     if (HomeContainer_ModuleThree_Content.style.display === "none")
-        HomeContainer_ModuleThree_Content.style.display = "flex";
+{    HomeContainer_ModuleThree_Content.style.display = "flex";
+    HomeContainer_ModuleThree_AttendanceDonut.style.display = "flex"}
     else
-        HomeContainer_ModuleThree_Content.style.display = "none";
+{        HomeContainer_ModuleThree_Content.style.display = "none";
+        HomeContainer_ModuleThree_AttendanceDonut.style.display = "none";}
 }
